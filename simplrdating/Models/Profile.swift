14f17f81707dc082921912a7cf9bd0fb66a91758
Brandon <contacto@senaamd.com>
2023-08-16 00:12:43 -0600

    UIUX Changes to SwipingView; Profile moved to Models

---
+++ simplrdating/Models/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  simplrdating
+//
+//  Created by Brandon Ramírez Casazza on 16/08/23.
+//
+
+import SwiftUI
+
+struct Profile: Identifiable, Equatable {
+    var id: Int
+    var imageName: String
+}
--- simplrdating/Views/Home (Swiping)/SwipingView.swift
+++ simplrdating/Views/Home (Swiping)/SwipingView.swift
@@ -20,7 +20,8 @@ struct SwipingView: View {
         // ... add more profiles as needed
     ]
     @State private var translation: CGSize = .zero
-
+    @State private var endTranslation: CGSize = .zero
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -28,29 +29,43 @@ struct SwipingView: View {
                 Spacer()
                 
                 ZStack {
-                    ForEach(profiles, id: \.id) { profile in
-                        // Check if the current profile is the last one in the array
+                    ForEach(profiles.indices.reversed(), id: \.self) { index in
+                        let profile = profiles[index]
+                        
                         if profile == profiles.last {
                             ProfileCard(profile: profile)
-                                .offset(x: translation.width, y: 0) // Only allow horizontal movement
-                                .rotationEffect(.degrees(Double(translation.width / 10))) // Adjust rotation based on swipe distance
-                                .animation(.interactiveSpring, value: 1.0)
+                                .offset(x: translation.width + endTranslation.width, y: endTranslation.height)
+                                .rotationEffect(.degrees(Double(translation.width / 10)))
+                                .opacity(opacityForTopCard())
+                                .scaleEffect(scaleForTopCard())
+                                .animation(.interactiveSpring(response: 0.3, dampingFraction: 0.6, blendDuration: 0), value: translation)
                                 .gesture(
                                     DragGesture()
                                         .onChanged { gesture in
                                             translation = gesture.translation
                                         }
-                                        .onEnded { _ in
-                                            handleSwipe()
+                                        .onEnded { value in
+                                            handleSwipe(with: value)
                                         }
                                 )
-                        } else {
-                            // Display other cards in the stack without the gesture
+                                .zIndex(Double(index))
+                        } else if profile == profiles[profiles.count - 2] {
                             ProfileCard(profile: profile)
+                                .opacity(1.0)
+                                .scaleEffect(1.0)
+                                .offset(y: 0)
+                                .animation(.easeInOut(duration: 0.4), value: profiles.count)
+                                .zIndex(Double(index))
+                        } else {
+                            ProfileCard(profile: profile)
+                                .opacity(0.85)
+                                .scaleEffect(0.9)
+                                .offset(y: -20)
+                                .zIndex(Double(index))
                         }
                     }
                 }
-                .frame(height:400)
+                .frame(height: 400)
                 .padding()
                 
                 Spacer()
@@ -59,79 +74,118 @@ struct SwipingView: View {
         }
     }
     
-    func handleSwipe() {
-        if translation.width < -100 {
-            // Swipe left (dislike)
-            profiles.removeLast()
-        } else if translation.width > 100 {
-            // Swipe right (like)
-            profiles.removeLast()
+    func opacityForTopCard() -> Double {
+        let defaultOpacity: Double = 1.0
+        let endOpacity: Double = 0.5
+        let progress = abs(translation.width) / UIScreen.main.bounds.width
+        return defaultOpacity - Double(progress) * (defaultOpacity - endOpacity)
+    }
+    
+    func scaleForTopCard() -> CGFloat {
+        let defaultScale: CGFloat = 1.0
+        let endScale: CGFloat = 0.3
+        
+        let progress = abs(translation.width) / UIScreen.main.bounds.width
+        return defaultScale - (progress * (defaultScale - endScale))
+    }
+    
+    func handleSwipe(with gesture: DragGesture.Value) {
+        let swipeThreshold: CGFloat = 80.0
+        let swipeSpeedThreshold: CGFloat = 700.0
+        
+        if gesture.translation.width > swipeThreshold || gesture.predictedEndTranslation.width/0.3 > swipeSpeedThreshold {
+            // Move card to Matches section of the bottom NavBar
+            withAnimation {
+                endTranslation = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height)
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                profiles.removeLast()
+                resetCardState()
+            }
+        } else if gesture.translation.width < -swipeThreshold || gesture.predictedEndTranslation.width/0.3 < -swipeSpeedThreshold {
+            // "Putting into Trash" effect
+            withAnimation {
+                endTranslation = CGSize(width: -UIScreen.main.bounds.width, height: 0)
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                profiles.removeLast()
+                resetCardState()
+            }
+        } else {
+            resetCardState()
         }
+    }
+    
+    func resetCardState() {
+        endTranslation = .zero
         translation = .zero
     }
-}
-
-struct ProfileCard: View {
-    var profile: Profile
-
-    var body: some View {
-        Image(profile.imageName)
-            .resizable()
-            .scaledToFill()
-            .frame(width: 300, height: 400)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .overlay(
-                LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.4)]), startPoint: .center, endPoint: .bottom)
-            )
-    }
-}
-
-// struct BottomNavBar: View {
-//    var body: some View {
-//        HStack(spacing: 50) {  // Adjust the spacing value as needed
-//            Button(action: {
-//                // Navigate to Profile
-//            }) {
-//                Image(systemName: "person.crop.circle.fill")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.gray)
-//            }
-//
-//            Button(action: {
-//                // Navigate to Main Swiping Area
-//            }) {
-//                Image(systemName: "flame.fill")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.red)
-//            }
-//
-//            NavigationLink(destination: MatchesView()) {
-//                Image(systemName: "message.circle.fill")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.gray)
-//            }
-//        }
-//        .padding(.horizontal)
-//    }
-//}
-
-struct Profile: Identifiable, Equatable {
-    var id: Int
-    var imageName: String
-}
-
-//struct SwipingView_Previews: PreviewProvider {
-//    static var previews: some View {
+    
+    struct ProfileCard: View {
+        var profile: Profile
         
-//        VStack {
-//            SwipingView()
-//                .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
-//                .previewDisplayName("iPhone 14")
-//        }
-//    }
-//}
-
-#Preview {
-    SwipingView()
+        var body: some View {
+            Image(profile.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300, height: 400)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .overlay(
+                    LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.4)]), startPoint: .center, endPoint: .bottom)
+                )
+        }
+    }
+    
+    // struct BottomNavBar: View {
+    //    var body: some View {
+    //        HStack(spacing: 50) {  // Adjust the spacing value as needed
+    //            Button(action: {
+    //                // Navigate to Profile
+    //            }) {
+    //                Image(systemName: "person.crop.circle.fill")
+    //                    .font(.largeTitle)
+    //                    .foregroundColor(.gray)
+    //            }
+    //
+    //            Button(action: {
+    //                // Navigate to Main Swiping Area
+    //            }) {
+    //                Image(systemName: "flame.fill")
+    //                    .font(.largeTitle)
+    //                    .foregroundColor(.red)
+    //            }
+    //
+    //            NavigationLink(destination: MatchesView()) {
+    //                Image(systemName: "message.circle.fill")
+    //                    .font(.largeTitle)
+    //                    .foregroundColor(.gray)
+    //            }
+    //        }
+    //        .padding(.horizontal)
+    //    }
+    //}
+    
+    struct Profile: Identifiable, Equatable {
+        var id: Int
+        var imageName: String
+    }
+    
+    //struct SwipingView_Previews: PreviewProvider {
+    //    static var previews: some View {
+    
+    //        VStack {
+    //            SwipingView()
+    //                .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
+    //                .previewDisplayName("iPhone 14")
+    //        }
+    //    }
+    //}
+    
+    //#Preview {
+    //    SwipingView()
+    //}
+    
 }
